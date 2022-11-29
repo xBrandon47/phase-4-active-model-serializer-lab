116759185412828a7dfbde10205a4c5933f4c504
Brandon Moreno <brandonm99@me.com>
2022-11-28 22:27:14 -0700

    commit tree 2c3dccb76ec6d0eb1ad9a1ece8aa5a75a872bb4f

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,7 +3,7 @@ class AuthorsController < ApplicationController
 
   def index
     authors = Author.all 
-    render json: authors
+    render json: authors, include: ['profile', 'posts', 'posts.tags']
   end
 
   def show
---
+++ app/serializers/posts_serializer.rb
@@ -0,0 +1,5 @@
+class PostsSerializer < ActiveModel::Serializer
+  attributes :title, :content 
+  belongs_to :author
+  has_many :tags
+end
